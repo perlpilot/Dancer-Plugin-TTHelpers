403da805045af40c4e86d8c562cdc563a9b2ba45
Jonathan Scott Duff <duff@pobox.com>
2012-10-02 14:58:22 -0500

    Use fully qualified sub name for HTML::FormHelpers::process_attributes

--- lib/Dancer/Plugin/TTHelpers.pm
+++ lib/Dancer/Plugin/TTHelpers.pm
@@ -153,7 +153,7 @@ hook 'before_template' => sub {
 
 
 sub css {
-    my $attributes = &process_attributes;
+    my $attributes = &HTML::FormHelpers::process_attributes;
     my ( $uri, $ie_cond ) = @_;
     $uri .= '.css' unless $uri =~ /\.css$/;
     return
@@ -165,7 +165,7 @@ sub css {
 }
 
 sub js {
-    my $attributes = &process_attributes;
+    my $attributes = &HTML::FormHelpers::process_attributes;
     my ( $uri, $ie_cond ) = @_;
     $uri .= '.js' unless $uri =~ /\.js$/;
     return
